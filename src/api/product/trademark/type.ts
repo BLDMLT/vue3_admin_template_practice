d50a1bde84fa353915b4365fa2e23eb6f87dfe0b
BLDMLT <hjzinau@gmail.com>
2023-11-13 08:34:06 +1030

    feat(main): add trademark types

--- src/api/product/trademark/type.ts
+++ src/api/product/trademark/type.ts
@@ -1,28 +1,28 @@
 // 品牌管理类型
 export interface ResponseData {
-  code: number,
-  message: string,
-  ok: boolean,
+  code: number
+  message: string
+  ok: boolean
 }
 
 // 已有品牌的ts数据类型
 export interface TradeMark {
-  id?: number,
-  tmName: string,
+  id?: number
+  tmName: string
   logoUrl: string
 }
 
 // 包含全部品牌数据的ts数据类型
-export type Records = TradeMark[];
+export type Records = TradeMark[]
 
 // 获取的已有品牌的数据的ts类型
 export interface TradeMarkResponseData extends ResponseData {
   data: {
-    records: Records,
-    total: number,
-    size: number,
-    current: number,
-    searchCount: boolean,
-    pages: number,
+    records: Records
+    total: number
+    size: number
+    current: number
+    searchCount: boolean
+    pages: number
   }
 }
